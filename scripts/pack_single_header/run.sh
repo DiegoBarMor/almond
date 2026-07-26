10bb40382de7bcbfe977a10e0ef33d8719ac924f
DBM <diegobarmor42@gmail.com>
2026-07-27 00:46:55 +0200

    added script for extracting system include directives from the merged headers
    moved around problematic declaration-definition

--- scripts/pack_single_header/run.sh
+++ scripts/pack_single_header/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-folder_tmp=$(mktemp -d)
+folder_tmp0=$(mktemp -d)
+folder_tmp1=$(mktemp -d)
 
-python3 scripts/pack_single_header/merge_cpp_hpp.py "almond" "$folder_tmp"
+python3 scripts/pack_single_header/merge_cpp_hpp.py "almond" "$folder_tmp0"
+python3 scripts/pack_single_header/pop_system_headers.py "$folder_tmp0" "$folder_tmp1" # creates "system_includes.txt"
